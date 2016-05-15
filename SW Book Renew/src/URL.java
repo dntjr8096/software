b76e0be868ef783f2fed8a2270c0d6d5eef4c273
dntjr8096 <lkjs8269@gmail.com>
2016-05-15 22:23:14 +0900

    XML기초부분 포함

---
+++ SW Book Renew/src/URL.java
@@ -0,0 +1,29 @@
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class URL {
+	private static final String BASE_URL = "http://www.aladdin.co.kr/ttb/api/ItemSearch.aspx?";
+
+	public static String GetUrl(String searchWord, String queryType) throws Exception {
+		Map<String,String> hm = new HashMap<String,String>();
+		hm.put("ttbkey", "ttblkjs82691117001");
+		hm.put("Query", URLEncoder.encode(searchWord, "UTF-8"));
+		hm.put("QueryType", URLEncoder.encode(queryType, "UTF-8"));
+		hm.put("MaxResults", "10");
+		hm.put("start", "1");
+		hm.put("SearchTarget", "Book");
+		hm.put("output", "xml");
+
+		StringBuffer sb = new StringBuffer();
+		Iterator<String> iter = hm.keySet().iterator();
+		while (iter.hasNext()) {
+			String key = iter.next();
+			String val  = hm.get(key);
+			sb.append(key).append("=").append(val).append("&");
+		}
+
+		return BASE_URL + sb.toString();
+	}
+}
---
+++ SW Book Renew/src/XMLReader.java
@@ -0,0 +1,30 @@
+
+public class XMLReader {
+
+	static private int class_num = 0;
+	static private XMLReader rxml;
+	private String searchWord, queryType;
+	
+	private XMLReader(){
+	}
+	
+	public static XMLReader newInstance(){
+		if(class_num == 0){
+			rxml = new XMLReader();
+			class_num++;
+			return rxml;
+		}
+		else{
+			return rxml;
+		}
+	}
+	
+	public void setSearchWord(String searchWord){
+		this.searchWord = searchWord;
+	}
+	
+	public void setQueryType(String queryType){
+		this.queryType = queryType;
+	}
+	
+}
