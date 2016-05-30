c0fd6bea6de0e28ec1fcc8506b58d41fecc6c53b
dntjr8096 <lkjs8269@gmail.com>
2016-05-30 21:18:00 +0900

    My Book Frame 작업중

--- SW Book Renew/src/view/MyBookFrame.java
+++ SW Book Renew/src/view/MyBookFrame.java
@@ -6,15 +6,21 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
+import model.Book;
+
 public class MyBookFrame extends JFrame{
 	private JButton add, delete;
 	private JScrollPane jsp;
-	private ArrayList<BookPanel> bookpanels;
+	private ArrayList<BookPanel> bookPanels;
 	
 	public MyBookFrame(){
 		add = new JButton("책 추가");
 		delete = new JButton("책 삭제");
+		bookPanels = new ArrayList<BookPanel>();
 		
 	}
 	
+	public void addBooks(ArrayList<Book> books){
+		books.
+	}
 }
