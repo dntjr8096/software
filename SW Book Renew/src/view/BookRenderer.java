eb41c5191cab2f1a9dda7c4d6a36af8b59d0c43d
dntjr8096 <lkjs8269@gmail.com>
2016-06-05 23:05:18 +0900

    renderer수정

--- SW Book Renew/src/view/BookRenderer.java
+++ SW Book Renew/src/view/BookRenderer.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Image;
 import java.net.MalformedURLException;
@@ -15,23 +16,31 @@ import model.Book;
 //JList에 한 칸을 Cell이라 하는데 그 Cell의 양식을 정하는 class List.setCellRender에서 사용
 public class BookRenderer extends DefaultListCellRenderer{
 	 public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {
-	        JLabel renderer = (JLabel)(super.getListCellRendererComponent(list, value, index, isSelected, hasFocus));
-	        if (value instanceof Book) {
-	            Book b = (Book)value;
-	            renderer.setText("제목: " + b.Title + "    저자: " + b.Author);
-	            ImageIcon temp;
-	            renderer.setName(b.Title);
-				try {
-					temp = new ImageIcon(new URL(b.Cover));
-					Image tempi = temp.getImage();		//ImageIcon을 Image로 변환
-					Image transi = tempi.getScaledInstance(250, 240, Image.SCALE_SMOOTH);
-					ImageIcon icon = new ImageIcon(transi);		//Image로 ImageIcon 생성
-					renderer.setIcon(icon);
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}	//temp를 resize할것임
-	        }
+//	        JLabel renderer = (JLabel)(super.getListCellRendererComponent(list, value, index, isSelected, hasFocus));
+//	        if (value instanceof Book) {
+//	            Book b = (Book)value;
+//	            renderer.setText("제목: " + b.Title + "    저자: " + b.Author);
+//	            ImageIcon temp;
+//	            renderer.setName(b.Title);
+//				try {
+//					temp = new ImageIcon(new URL(b.Cover));
+//					Image tempi = temp.getImage();		//ImageIcon을 Image로 변환
+//					Image transi = tempi.getScaledInstance(250, 240, Image.SCALE_SMOOTH);
+//					ImageIcon icon = new ImageIcon(transi);		//Image로 ImageIcon 생성
+//					renderer.setIcon(icon);
+//				} catch (MalformedURLException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}	//temp를 resize할것임
+//	        }
+//	        return renderer;
+		 //수정이후
+		 BookPanel renderer = (BookPanel) value;
+	        renderer.setBackground(isSelected ? new Color(0,0,255,30) : Color.WHITE);
+	        renderer.getAuthor().setBackground(isSelected ? new Color(0,0,255,30) : Color.WHITE);
+	        renderer.getAuthor().setOpaque(true);
+	        renderer.getTitle().setBackground(isSelected ? new Color(0,0,255,30) : Color.WHITE);
+	        renderer.getTitle().setOpaque(true);
 	        return renderer;
 	 }
 }
